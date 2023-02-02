6f8be2b00fea12e23d479b1eb9519ae009d5d42a
Ali Kefia <ali.kefia@gmail.com>
2023-02-02 15:12:15 +0100

    log experiences to a csv file

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
+import csv
+import os
 import resource
+from collections import namedtuple
 from datetime import datetime
 from functools import wraps
+from pathlib import Path
 
 cols = {
     "id_mutation": "str",
@@ -46,26 +50,55 @@ cols = {
 }
 
 
+Experience = namedtuple(
+    "Experience", ("mod", "fn", "ctx", "tm", "u_cpu", "s_cpu", "max_mem")
+)
+
+
+def print_stats(e: Experience):
+    print(
+        f"""
+Resources usage:
+- exec time     : {e.tm}s
+- user cpu time : {e.u_cpu}s
+- sys cpu time  : {e.s_cpu}s
+- max mem usage : {e.max_mem}GB
+        """
+    )
+
+
+def log_stats(e: Experience):
+    new = False
+    if not Path(os.environ["RES"]).exists():
+        new = True
+    with open(os.environ["RES"], "a") as fd:
+        writer = csv.writer(fd)
+        if new is True:
+            writer.writerow(Experience._fields)
+        writer.writerow((e.mod, e.fn, e.ctx, e.tm, e.u_cpu, e.s_cpu, e.max_mem))
+
+
 def with_res_logger(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         start = datetime.now()
         res = f(*args, **kwargs)
-        t = datetime.now() - start
+        end = datetime.now()
         me = resource.getrusage(resource.RUSAGE_SELF)
         children = resource.getrusage(resource.RUSAGE_CHILDREN)
-        u_cpu = me.ru_utime + children.ru_utime
-        s_cpu = me.ru_stime + children.ru_stime
-        max_mem = me.ru_maxrss + children.ru_maxrss
-        print(
-            f"""
-Resource consumption:
-- exec time     : {t.total_seconds():.2f}s
-- user cpu time : {u_cpu:.2f}s
-- sys cpu time  : {s_cpu:.2f}s
-- max mem usage : {max_mem / (1024*1024*1024):.2f}GB
-            """
+        e = Experience(
+            tm=f"{(end - start).total_seconds():.2f}",
+            mod=f.__module__,
+            fn=f.__name__,
+            ctx="|".join(
+                [str(arg) for arg in args] + [str(kwarg) for kwarg in kwargs.values()]
+            ),
+            u_cpu=f"{me.ru_utime + children.ru_utime:.2f}",
+            s_cpu=f"{me.ru_stime + children.ru_stime:.2f}",
+            max_mem=f"{(me.ru_maxrss + children.ru_maxrss)/(1024*1024*1024):.2f}",
         )
+        log_stats(e)
+        print_stats(e)
         return res
 
     return wrapper
