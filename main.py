94580616de0406e716f5262d9bfdb4cc2e9ab17a
Sami Bargaoui <bargaoui.sam@gmail.com>
2023-03-12 17:47:18 +0100

    * 🆕 feat(app_modin.py): add support for Modin Pandas This commit adds support for Modin Pandas, a library that allows for parallel and distributed computing of Pandas dataframes. The `modin.pandas` library is imported and used in place of the standard `pandas` library. The `ray` library is also imported and initialized to allow for distributed computing. The `load` function is modified to use Modin Pandas to read in CSV files. The `top_flop` function is also modified to use Modin Pandas to perform groupby operations on the loaded data.
    * ✨ feat(main.py): add support for Modin library
    The Modin library is now supported in the application. The `app_modin` module has been imported and added as a subcommand to the `app` command. This allows users to use Modin as a backend for their data processing tasks.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import typer  # noqa: E402
 from app_dask import app as dask_app  # noqa: E402
 from app_duckdb import app as duckdb_app  # noqa: E402
 from app_explore import app as explore_app  # noqa: E402
+from app_modin import app as modin_app  # noqa: E402
 from app_pandas import app as pandas_app  # noqa: E402
 from app_polars import app as polars_app  # noqa: E402
 from utils import with_res_logger  # noqa: E402
@@ -43,6 +44,7 @@ app.add_typer(pandas_app, name="pandas")
 app.add_typer(dask_app, name="dask")
 app.add_typer(polars_app, name="polars")
 app.add_typer(duckdb_app, name="duckdb")
+app.add_typer(modin_app, name="modin")
 
 
 if __name__ == "__main__":
